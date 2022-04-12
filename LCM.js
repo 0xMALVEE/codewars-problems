d21f2354eb0ab4180a7ba1d425a3b90af1ed7d32
Alvee <M.Alvee8141@gmail.com>
2022-04-12 17:59:37 +0600

    +Adding Array Problem

---
+++ LCM.js
@@ -0,0 +1,15 @@
+// var lcm = function () {
+//     let result = 0
+//     for(let i= 0;i< arguments.length; i++){
+//         if(arguments[i] / 2 != 1){
+//             result += arguments[i]
+//         }else{
+//             result += arguments[i] / 2
+//         }
+        
+//     }
+
+//     return result
+// };
+
+// console.log(lcm(2,3,4))
